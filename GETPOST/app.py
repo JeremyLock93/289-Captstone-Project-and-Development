c0fc79956f45db23f0e02c1d1fb515f78fd670de
LordAllan51 <74980052+LordAllan51@users.noreply.github.com>
2022-02-27 16:47:39 -0500

    Add files via upload

--- GETPOST/app.py
+++ GETPOST/app.py
@@ -1,17 +1,33 @@
 try:
-
-    from flask import Flask
-
-    from flask import redirect, url_for, request, render_template, send_file
     from io import BytesIO
 
+    from flask import (Flask, redirect, render_template, request, send_file,
+                       url_for)
+    from flask_wtf import Form
     from flask_wtf.file import FileField
     from wtforms import SubmitField
-    from flask_wtf import Form
-    import sqlite3
     print("All Modules Loaded .... ")
 except:
     print (" Some Module are missing ...... ")
+
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='database',
+                                         user='user',
+                                         password='password', use_pure=True)
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL database... MySQL Server version on ", db_Info)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed")
 
 
 app = Flask(__name__)
