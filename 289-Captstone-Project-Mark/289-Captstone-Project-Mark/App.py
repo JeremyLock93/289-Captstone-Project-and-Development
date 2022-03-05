12ea4a949ccce2aaf947ab7883c7a036b2c5f60f
Mark Putnam <70597443+PutnamM3196@users.noreply.github.com>
2022-03-05 17:48:38 -0500

    Add files via upload

--- 289-Captstone-Project-Mark/289-Captstone-Project-Mark/App.py
+++ 289-Captstone-Project-Mark/289-Captstone-Project-Mark/App.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return render_template("index.html")
-
-
+@app.route("/homepage")
+def homepage():
+	return render_template("homepage.html")
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
