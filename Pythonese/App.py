b815d527e5f5fe6867c397b279e98b22fc0dc2dc
JeremyLock93 <78815263+JeremyLock93@users.noreply.github.com>
2022-04-18 20:27:07 -0400

    Changes to the main code and creation of the database

--- Pythonese/App.py
+++ Pythonese/App.py
@@ -7,36 +7,46 @@ app = Flask(__name__)
 
 
 
-@app.route("/home")
-@app.route("/")
+@app.route("/home", methods = ['GET', 'POST'])
+@app.route("/", methods = ['GET', 'POST'])
 def index():
 	return render_template("index.html")
 
 
-@app.route("/calendar")
+@app.route("/calendar", methods = ['GET', 'POST'])
 def calendar():
     return render_template("calendar.html")
 
 
-@app.route("/templates")
+@app.route("/templates", methods = ['GET', 'POST'])
 def templates():
     return render_template("templates.html")
 
 
-@app.route("/profile")
+@app.route("/profile", methods = ['GET', 'POST'])
 def profile():
-    return render_template("profile.html")
+        return render_template("profile.html")
+      
 
-
-@app.route("/loggin")
+@app.route("/loggin", methods = ['GET', 'POST'])
 def loggin():
-    return render_template("loggin.html")
+        return render_template("loggin.html")
+        if request.method == 'POST':
+            session['username'] = request.form ['username']
+            return redirect(url_for('home'))
+        return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''   
+        
 
 
-@app.route("/sign-up")
+@app.route("/sign-up", methods = ['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
-
+    
 
 
 if __name__ == "__main__":
