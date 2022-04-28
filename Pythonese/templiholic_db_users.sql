b681b884bdfa868c8a168b7a673ad09ab7732b75
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2022-04-27 21:10:09 -0400

    Add files via upload

---
+++ Pythonese/templiholic_db_users.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `templiholic_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `templiholic_db`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: templiholic_db
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `USID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(15) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `FirstName` varchar(15) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `Affiliation` char(1) NOT NULL,
+  `JoinDate` datetime NOT NULL,
+  `TID` int DEFAULT NULL,
+  `FID` int DEFAULT NULL,
+  PRIMARY KEY (`USID`),
+  UNIQUE KEY `USID_UNIQUE` (`USID`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`),
+  KEY `TID_idx` (`TID`),
+  KEY `FID_idx` (`FID`),
+  CONSTRAINT `FID` FOREIGN KEY (`FID`) REFERENCES `files` (`FID`),
+  CONSTRAINT `TID` FOREIGN KEY (`TID`) REFERENCES `templates` (`TID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-27 20:55:15
