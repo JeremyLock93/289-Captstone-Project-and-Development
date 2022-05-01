a5d15826decae3f459ee90fa7aef7265863cb7d3
Mark Putnam <70597443+PutnamM3196@users.noreply.github.com>
2022-04-30 22:17:24 -0400

    Add files via upload

--- Mark_289/Pythonese_working_directory/App.py
+++ Mark_289/Pythonese_working_directory/App.py
@@ -1,11 +1,19 @@
 import sys
 import io
 import os
-from flask import Flask, redirect, url_for, render_template, request, send_file
+from flask import Flask, flash, redirect, url_for, render_template, request, send_file
+from werkzeug.utils import secure_filename
+from Test import Test
+
+
+UPLOAD_FOLDER = 'static/user_info'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 USER_DATA = os.path.join('static','user_data')
 
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['user_info'] = USER_DATA
 
 @app.route("/home")
@@ -23,21 +31,49 @@ def calendar():
 def templates():
     return render_template("templates.html")
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/profile")
 def profile():
+    
     PROFILE_PIC = os.path.join(app.config['user_info'], 'profile_pic.jpg')
-    USERNAME = os.path.join(app.config['user_info'], 'user_info.json')
+    '''USERNAME = os.path.join(app.config['user_info'], 'user_info.json')
     NAME = os.path.join(app.config['user_info'], 'user_info.json')
     EMAIL = os.path.join(app.config['user_info'], 'user_info.json')
-    PASSWORD = os.path.join(app.config['user_info'], 'user_info.json')
+    PASSWORD = os.path.join(app.config['user_info'], 'user_info.json')'''
+    userRecord = Test()
+
+
     return render_template("profile.html",
         profile_pic = PROFILE_PIC,
-        username = USERNAME,
-        name = NAME,
-        email = EMAIL,
-        password = PASSWORD)
-    
+        username = userRecord[1],
+        name = userRecord[3] +" "+ userRecord[2],
+        email = userRecord[4],
+        password = userRecord[5])
+
+
+
+
+@app.route("/upload-image", methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return 
 
 
 
