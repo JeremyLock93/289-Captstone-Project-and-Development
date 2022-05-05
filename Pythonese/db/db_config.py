b69ebbbd8995d025af9d14589c9e845c8648967b
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2022-05-05 05:09:46 -0400

    Delete Pythonese/db directory

--- Pythonese/db/db_config.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-def connection():
-    cnx = MySQLdb.connect(user = 'root', 
-                              password = '',
-                              host = 'localhost', 
-                              database = 'Templholic_DB')
-
-    c = cnx.cursor()
