0de0111f77cd5eb7ff7cc78b267b22886c34ff37
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2022-05-09 02:00:43 -0400

    Add files via upload

---
+++ db/templiholics_db_templatedata.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: templiholics_db
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `templatedata`
+--
+
+DROP TABLE IF EXISTS `templatedata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `templatedata` (
+  `AID` int NOT NULL AUTO_INCREMENT,
+  `TID` int NOT NULL,
+  `AssignmentName` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8_swedish_ci NOT NULL,
+  `DueDate` varchar(35) COLLATE utf8_swedish_ci NOT NULL,
+  `Comments` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`AID`),
+  KEY `TID_idx` (`TID`),
+  KEY `TIDs_idx` (`TID`),
+  CONSTRAINT `TIDs` FOREIGN KEY (`TID`) REFERENCES `templates` (`TID`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `templatedata`
+--
+
+LOCK TABLES `templatedata` WRITE;
+/*!40000 ALTER TABLE `templatedata` DISABLE KEYS */;
+/*!40000 ALTER TABLE `templatedata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-08 18:04:07
